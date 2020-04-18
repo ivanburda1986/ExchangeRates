ac1efda6d419363d73067404bc0f5fd2e13aace2
ivanburda1986 <burda.ivan@gmail.com>
2020-04-18 23:52:42 +0200

    fetch currencies, do conversion, output the result

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,3 @@
-//Initialize the Fetch request for the exchange rates
-const xr = new XR;
-
-
-
 //Initialize the UI
 const ui = new UI();
 
@@ -10,12 +5,29 @@ const ui = new UI();
 const listTarget1 = document.getElementById('currencySelect1');
 const listTarget2 = document.getElementById('currencySelect2');
 
-//Get exchange rates
-const exchangeRates = xr.getExchangeRates()
+//Initialize the class with methods related to the currency conversions
+const xr = new XR();
+
+//Get currencies and populate with them the selection lists
+const currencies = xr.getCurrencies()
   .then(response => {
     ui.listCurrencies(listTarget1, response);
     ui.listCurrencies(listTarget2, response)
   })
   .catch(err => console.log(err));
 
-//Get currency names https://currencylayer.com/documentation
+
+//Request a conversion -- Unfortunately not supported by the Free Plan
+// const conversion = xr.convert('USD', 'GBP', 1)
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => console.log(err));
+
+
+//Convert an amount
+const conversion = xr.convert('CZK', 'PLN', 100)
+  .then(response => {
+    ui.displayConversionResult(response);
+  })
+  .catch(err => console.log(err));
--- dist/ui.js
+++ dist/ui.js
@@ -1,20 +1,30 @@
+function trimName(input) {
+  let outcome = '';
+  outcome = input.replace(/"/g, '').replace(/,/g, ' - ');
+  return outcome;
+}
+
+
 class UI {
   constructor() {
-
+    this.conversionResultOutput = document.getElementById('result');
   }
-
   listCurrencies(listTarget, response) {
     //Set the target where to place the list of currencies - the target gets inserted as parameter of the constructor
-    let currencies = Object.keys(response.exchangeRates.rates);
-
+    let currencies = Object.entries(response.currencies.currencies);
     let listOptions = '';
     let value = 1;
+
     currencies.forEach(function (currency) {
       listOptions += `
-      <option value="${value}">"${currency}"</option>
+      <option value="${value}">${currency[1]} (${currency[0]})</option>
       `;
       value += 1;
     });
     listTarget.innerHTML = listOptions;
+  }
+
+  displayConversionResult(response) {
+    this.conversionResultOutput.innerHTML = `${response.conversionResult}`;
   }
 }
