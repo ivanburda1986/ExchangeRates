6fe54890cf8bd3f1f3e914262b6d2bffed9ef3af
ivanburda1986 <burda.ivan@gmail.com>
2020-04-25 14:43:57 +0200

    progressing with the styling

--- dist/ui.js
+++ dist/ui.js
@@ -1,19 +1,18 @@
 // Currency flags: https://fabian7593.github.io/CountryAPI/
 
-
 class UI {
   constructor() {
-    this.conversionResultOutput = document.getElementById('amountOutput1');
-    this.exchangeRateOutput = document.getElementById('exchangeRate');
-    this.currencySelect1 = document.getElementById('currencySelect1');
-    this.currencySelect2 = document.getElementById('currencySelect2');
-    this.amountInput = document.getElementById('amountInput1');
+    this.conversionResultOutput = document.getElementById("amountOutput1");
+    this.exchangeRateOutput = document.getElementById("exchangeRate");
+    this.currencySelect1 = document.getElementById("currencySelect1");
+    this.currencySelect2 = document.getElementById("currencySelect2");
+    this.amountInput = document.getElementById("amountInput1");
   }
 
   //Display in the UI (listTarget) the of currencies (response)
   listCurrencies(listTarget, response) {
     let currencies = Object.entries(response.currencies);
-    let listOptions = '';
+    let listOptions = "";
     currencies.forEach(function (currency) {
       listOptions += `
       <option value="${currency[0]}">${currency[1]} (${currency[0]})</option>
@@ -24,13 +23,17 @@ class UI {
 
   //Get the FROM currency, TO currency and the AMOUNT to convert
   getConversionInput() {
-    const fromCurrency = this.currencySelect1.options[this.currencySelect1.selectedIndex].value;
-    const toCurrency = this.currencySelect2.options[this.currencySelect2.selectedIndex].value;
+    const fromCurrency = this.currencySelect1.options[
+      this.currencySelect1.selectedIndex
+    ].value;
+    const toCurrency = this.currencySelect2.options[
+      this.currencySelect2.selectedIndex
+    ].value;
     const amountToConvert = this.amountInput.value;
     return {
       fromCurrency,
       toCurrency,
-      amountToConvert
+      amountToConvert,
     };
   }
 
